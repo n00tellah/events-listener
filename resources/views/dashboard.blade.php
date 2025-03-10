d74c3acfa4e46a19c69038ac27361e9afa1f4e71
Your Estela <jalacestelamae@gmail.com>
2025-03-10 17:10:35 +0800

    commit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,9 +11,20 @@
                 <div class="p-6 text-gray-900">
                     {{ __("You're logged in!") }}
                 </div>
-
                 <div>
-
+                    
+                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    @if (auth()->user()->role == 'admin')
+                    <div class="p-6 text-gray-900">
+                        <?php return view('admin-dashboard'); ?>
+                    </div>
+                        @elseif (auth()->user()->role == 'user')
+                        <div class="p-6 text-gray-900">
+                            {{ __("You're logged in!") }}
+                        </div>
+                    @endif
+                </div>
                 </div>
             </div>
         </div>
